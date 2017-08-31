501e71c08bc0bd28d1b65d3c6c000ff903c6a22a
lanshifu <1101460241@qq.com>
2017-08-31 08:20:55 +0800

    20170830

---
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/base/BaseToorBarActivity.java
@@ -0,0 +1,13 @@
+package me.goldze.mvvmhabit.base;
+
+import android.databinding.ViewDataBinding;
+
+/**
+ * Created by lanxiaobin on 2017/8/30.
+ */
+
+public abstract class BaseToorBarActivity<V extends ViewDataBinding, VM extends BaseViewModel> extends BaseActivity {
+
+
+
+}
---
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/http/BaseResponse.java
@@ -0,0 +1,39 @@
+package me.goldze.mvvmhabit.http;
+
+/**
+ * Created by goldze on 2017/5/10.
+ * 网络返回基类 支持泛型
+ */
+public class BaseResponse<T> {
+    private int code;
+    private String message;
+    private T result;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public boolean isOk() {
+        return code == 0;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
